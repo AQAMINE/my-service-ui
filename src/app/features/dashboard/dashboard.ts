866797ab8939d35bbf9254b1d728f5a09db13f53
aqamine <aqebliamine1@gmail.com>
2026-08-16 03:35:04 +0100

    service page dashboard

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,55 +1,16 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { AuthService } from '../../core/services/auth';
-import { environment } from '../../../environments/environment';
+import { AppTile } from '../../shared/components/app-tile/app-tile';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [AppTile],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
-export class Dashboard implements OnInit {
-  private http = inject(HttpClient);
+export class Dashboard {
   private authService = inject(AuthService);
-
-  // Signals pour gérer l'état UI
-  apiResponse = signal<string | null>(null);
-  isLoading = signal<boolean>(false);
-  errorMessage = signal<string | null>(null);
-
-  ngOnInit(): void {
-    this.testPrivateEndpoint();
-  }
-
-  /**
-   * Effectue un GET sur l'endpoint privé.
-   * L'intercepteur JWT ajoutera automatiquement le header Authorization.
-   */
-  testPrivateEndpoint(): void {
-    this.isLoading.set(true);
-    this.errorMessage.set(null);
-
-    this.http.get(`${environment.apiUrl}/private/hello`, { responseType: 'text' }).subscribe({
-      next: (data) => {
-        this.isLoading.set(false);
-        this.apiResponse.set(data);
-      },
-      error: (err) => {
-        this.isLoading.set(false);
-        console.error('Erreur lors de l\'appel de l\'endpoint privé:', err);
-        if (err.status === 401) {
-          this.errorMessage.set('Non autorisé (401) : Token invalide ou expiré.');
-        } else if (err.status === 403) {
-          this.errorMessage.set('Accès interdit (403) : Rôles insuffisants.');
-        } else {
-          this.errorMessage.set(`Erreur serveur (${err.status}) ou problème CORS.`);
-        }
-      }
-    });
-  }
 
   logout(): void {
     this.authService.logout();
