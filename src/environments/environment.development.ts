f71f9579dd934eacaa934f3f700ac1ac46529f16
aqamine <aqebliamine1@gmail.com>
2026-08-24 00:29:21 +0100

    environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: '/api'
+    apiUrl: 'http://localhost:8081/api',           // my-service principal sur 8081
+    cardsApiUrl: 'http://localhost:8083/api'
   };
